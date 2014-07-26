39a094db3a660a936b7d69b2633f405d493be04e
Andy Russell <arussell123@gmail.com>
2014-07-26 11:35:55 -0700

    rollback database on internal server error

--- gamefolk_shop/__init__.py
+++ gamefolk_shop/__init__.py
@@ -1,3 +1,5 @@
+import http.client
+
 from flask import Flask
 from flask.ext.login import LoginManager
 from flask.ext.sqlalchemy import SQLAlchemy
@@ -8,6 +10,12 @@ app.config.from_envvar('GAMEFOLKSHOP_SETTINGS')
 
 db = SQLAlchemy(app)
 login_manager = LoginManager(app)
+
+@app.errorhandler(http.client.INTERNAL_SERVER_ERROR)
+def server_error_handler(error):
+    """Cleanup the database and show an error page."""
+    db.session.rollback()
+    return 'Internal Server Error.', http.client.INTERNAL_SERVER_ERROR
 
 # Register blueprints. This must be done after the database is created.
 from gamefolk_shop.general.views import mod as general_module
