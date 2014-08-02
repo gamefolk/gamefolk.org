41288b402b12222aa15133c4889c27d977d8628e
Andy Russell <arussell123@gmail.com>
2014-08-02 11:14:44 -0700

    add foundation js to assets

--- gamefolk_shop/assets.py
+++ gamefolk_shop/assets.py
@@ -1,10 +1,13 @@
 from flask.ext.assets import Bundle, Environment
 
 js = Bundle(
-    'foundation/js/foundation.js'
+    'foundation/js/vendor/jquery.js',
+    'foundation/js/vendor/modernizr.js',
+    'foundation/js/foundation.min.js'
 )
 
 css = Bundle(
+    'foundation/css/normalize.css',
     'foundation/css/foundation.css'
 )
 
