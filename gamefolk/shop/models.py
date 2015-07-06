640c062d271e256e2240c17b96d28d2fd4365900
Andy Russell <arussell123@gmail.com>
2015-07-06 17:37:31 -0500

    add Stripe purchase option
    Closes #2.

--- gamefolk/shop/models.py
+++ gamefolk/shop/models.py
@@ -1,16 +1,35 @@
+"""Views for the shop."""
+
+from enum import Enum, unique
+
 from gamefolk import db
+
+# pylint: disable=invalid-name, too-few-public-methods
+
+
+@unique
+class PaymentSource(Enum):
+    """Payment handlers that the application supports."""
+    paypal = 'PayPal'
+    stripe = 'Stripe'
 
 
 class Transaction(db.Model):
-    """A completed PayPal transaction."""
-    id = db.Column(db.Integer, primary_key=True)        # pylint: disable=C0103
+    """A completed transaction for an item bought in the shop."""
 
-    # The transaction ID provided by PayPal
-    transaction_id = db.Column(db.String(20), unique=True)
+    id = db.Column(db.Integer, primary_key=True)
+
+    # The transaction ID provided by the payment handler
+    transaction_id = db.Column(db.String(20))
 
     # The user who completed the transaction
     user_id = db.Column(db.Integer)
 
-    def __init__(self, transaction_id, user_id):
+    # pylint: disable=no-member
+    payment_source = db.Column(db.Enum(*PaymentSource.__members__.keys()))
+    # pylint: enable=no-member
+
+    def __init__(self, transaction_id, user_id, payment_source):
         self.transaction_id = transaction_id
         self.user_id = user_id
+        self.payment_source = payment_source
--- gamefolk/shop/views.py
+++ gamefolk/shop/views.py
@@ -4,25 +4,34 @@ from functools import wraps
 import http
 from itertools import chain
 import json
-import logging
 
 import flask
-from flask import Blueprint, flash, request, render_template
-from flask.ext.login import login_required
+from flask import Blueprint, flash, redirect, request, render_template, url_for
+from flask.ext.security.core import current_user
+from flask.ext.security.decorators import login_required
 import requests
+import stripe
 from werkzeug.datastructures import ImmutableOrderedMultiDict
 
 from gamefolk import app, db
-from gamefolk.shop.models import Transaction
+from gamefolk.shop.models import PaymentSource, Transaction
 from gamefolk.users.models import User, create_secret_code
+
+# pylint: disable=invalid-name
 
 mod = Blueprint('shop', __name__, url_prefix='/shop')
 
 IPN_VERIFY_PARAMS = (('cmd', '_notify-validate'),)
+
+# Set up payment details
 if app.debug:
     IPN_URL = 'https://www.sandbox.paypal.com/cgi-bin/webscr'
+    stripe.api_key = app.config['STRIPE_TEST_SECRET_KEY']
+    STRIPE_PUBLISHABLE_KEY = app.config['STRIPE_TEST_PUBLISHABLE_KEY']
 else:
     IPN_URL = 'https://www.paypal.com/cgi-bin/webscr'
+    stripe.api_key = app.config['STRIPE_SECRET_KEY']
+    STRIPE_PUBLISHABLE_KEY = app.config['STRIPE_PUBLISHABLE_KEY']
 
 
 @mod.route('/')
@@ -36,7 +45,10 @@ def shop():
 def purchase():
     """The view that redirects the user to PayPal if they wish to buy a
     cartridge."""
-    return render_template('shop/purchase.html')
+    cartridge_cost = app.config['CARTRIDGE_COST']
+    return render_template('shop/purchase.html',
+                           cartridge_cost=cartridge_cost,
+                           stripe_key=STRIPE_PUBLISHABLE_KEY)
 
 
 @mod.route('/complete-payment')
@@ -45,6 +57,26 @@ def complete_payment():
     """Route to visit after completed payment."""
     flash('Payment completed!', 'success')
     return render_template('shop/complete_payment.html')
+
+
+@mod.route('/charge', methods=['POST'])
+@login_required
+def stripe_charge():
+    """Handles transactions from Stripe"""
+    cartridge_cost = app.config['CARTRIDGE_COST']
+
+    customer = stripe.Customer.create(
+        email=request.form['stripeEmail'],
+        card=request.form['stripeToken']
+    )
+
+    stripe.Charge.create(customer=customer.id,
+                         amount=cartridge_cost,
+                         currency='usd',
+                         description='Cartridge Purchase',
+                         metadata={'user_id': current_user.id})
+
+    return redirect(url_for('shop.complete_payment'))
 
 
 def ordered_storage(route):
@@ -57,22 +89,39 @@ def ordered_storage(route):
     return decorator
 
 
-def record_transaction(transaction_id, user_id):
-    """Store a successful PayPal transaction and generate a user's secret
-    code."""
+def record_transaction(transaction_id, user_id, payment_source):
+    """
+    Store a successful transaction and generate a user's secret code.
+
+    Parameters
+    ----------
+    transaction_id : str
+        A unique identifier of the transaction.
+
+    user_id : str
+        The id of the user that made the transaction.
+
+    payment_source : PaymentSource
+        The payment provider that handled the transaction.
+    """
     existing_transaction = Transaction.query.filter_by(
-        transaction_id=transaction_id).first()
+        transaction_id=transaction_id,
+        payment_source=payment_source.value).first()
     if existing_transaction:
-        logging.warning('Duplicate transaction {id} encountered',
-                        id=transaction_id)
+        app.logger.warning('Duplicate transaction %s encountered',
+                           transaction_id)
         return json.dumps({'status': 'failure'})
-    transaction = Transaction(transaction_id=transaction_id, user_id=user_id)
+    transaction = Transaction(transaction_id=transaction_id,
+                              user_id=user_id,
+                              payment_source=payment_source.value)
     db.session.add(transaction)
     db.session.commit()
-    purchasing_user = User.query.filter_by(id=int(user_id)).first()
+    purchasing_user = User.query.filter_by(id=int(user_id)).one()
     purchasing_user.secret_code = create_secret_code()
     db.session.commit()
-    logging.info('Complete transaction {id} recorded', id=transaction_id)
+    app.logger.info('Complete %s transaction %s recorded',
+                    payment_source.value,
+                    transaction_id)
 
 
 def validate_ipn(response, ipn_message):
@@ -80,6 +129,21 @@ def validate_ipn(response, ipn_message):
     intended_recipient = app.config['PAYPAL_MERCHANT_EMAIL']
     return (response.text == 'VERIFIED' and
             ipn_message['receiver_email'] == intended_recipient)
+
+
+@mod.route('/stripe-webhooks', methods=['POST'])
+def stripe_webhook():
+    """Webhook for completed Stripe charges."""
+    event = request.json
+    app.logger.debug('Received stripe webhook: %s',
+                     json.dumps(event, indent=4, sort_keys=True))
+
+    if event['type'] == 'charge.succeeded':
+        transaction_id = event['data']['object']['id']
+        user_id = event['data']['object']['metadata']['user_id']
+        record_transaction(transaction_id, user_id, PaymentSource.stripe)
+
+    return ('', http.client.NO_CONTENT)
 
 
 @mod.route('/ipn', methods=['POST'])
@@ -90,13 +154,14 @@ def instant_payment_notification():
     verify_params = chain(ipn_message.items(), IPN_VERIFY_PARAMS)
     response = requests.post(IPN_URL, params=dict(verify_params))
     if not validate_ipn(response, ipn_message):
-        logging.warning('PayPal IPN {arg} did not validate', arg=ipn_message)
+        app.logger.warning('PayPal IPN {arg} did not validate',
+                           arg=ipn_message)
         return json.dumps({'status': 'failure'})
 
     if ipn_message['payment_status'] == 'Completed':
         transaction_id = ipn_message['txn_id']
         user_id = ipn_message['custom']
-        record_transaction(transaction_id, user_id)
+        record_transaction(transaction_id, user_id, PaymentSource.paypal)
 
     return json.dumps({'status': 'complete'})
 
